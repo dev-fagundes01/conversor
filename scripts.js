12dbdb2a7ae56020359603c33af6ed9c3d54e6b7
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2023-02-19 18:59:41 -0300

    Adcionando API

--- scripts.js
+++ scripts.js
@@ -1,22 +1,21 @@
 const button = document.getElementById('convert-button')
 
-const dolar=5.2
-const euro=5.9
-const Bitcoin=105.237
 
-const convertValues=()=>{
+const convertValues= async ()=>{
     const inputReais=document.getElementById('input').value
     const realValueText=document.getElementById('real-value-text')
     const currencyValueText=document.getElementById('currency-value-text')
-    
-    //realValueText.innerHTML=inputReais
+   
+    const data= await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL').then(Response => Response.json())
+
+    const dolar=data.USDBRL.high
+    const euro=data.EURBRL.high
+    const bitcoin=data.BTCBRL.high
 
     realValueText.innerHTML=new Intl.NumberFormat("pt-BR", {
     style: "currency", 
     currency: "BRL"
     }).format(inputReais)
-
-    //currencyValueText.innerHTML=inputReais/dolar
 
     if(select.value==='US$ Dolar americano')
     currencyValueText.innerHTML=new Intl.NumberFormat("en-US", {
@@ -34,7 +33,7 @@ const convertValues=()=>{
     currencyValueText.innerHTML=new Intl.NumberFormat("en-US", {
     style: "currency", 
     currency: "BTC"
-    }).format(inputReais/Bitcoin)
+    }).format(inputReais/bitcoin)
 }
 
 button.addEventListener("click", convertValues)
