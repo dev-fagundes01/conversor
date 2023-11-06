0c6ca0758724090374a0c42ca12a3777fe661707
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2023-11-06 08:24:33 -0300

    Correção na conversão do dolar

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 const button = document.getElementById('convert-button')
 
 
-const convertValues= async ()=>{
+const convertValues= async () => {
     const inputReais=document.getElementById('input').value
     const realValueText=document.getElementById('real-value-text')
     const currencyValueText=document.getElementById('currency-value-text')
    
     const data= await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL').then(Response => Response.json())
 
-    const dólar=data.USDBRL.high
+    const dolar=data.USDBRL.high
     const euro=data.EURBRL.high
     const bitcoin=data.BTCBRL.high
 
@@ -17,19 +17,19 @@ const convertValues= async ()=>{
     currency: "BRL"
     }).format(inputReais)
 
-    if(select.value==='US$ Dólar americano')
-    currencyValueText.innerHTML=new Intl.NumberFormat("en-US", {
-    style: "currency", 
-    currency: "USD"
-    }).format(inputReais/dólar)
-
-    if(select.value==='€ Euro')
+    if(select.value==='euro')
     currencyValueText.innerHTML=new Intl.NumberFormat("de-DE", {
     style: "currency", 
     currency: "EUR"
     }).format(inputReais/euro)
 
-    if(select.value==='Bitcoin')
+    if(select.value==='dolar')
+    currencyValueText.innerHTML=new Intl.NumberFormat("en-US", {
+    style: "currency", 
+    currency: "USD"
+    }).format(inputReais/dolar)
+
+    if(select.value==='bitcoin')
     currencyValueText.innerHTML=new Intl.NumberFormat("en-US", {
     style: "currency", 
     currency: "BTC"
@@ -44,15 +44,15 @@ changeCurrency= () => {
     const currencyName = document.getElementById("currency-name")
     const currencyImg = document.getElementById("currency-img")
 
-    if(select.value==='US$ Dólar americano'){
+    if(select.value==='dolar'){
         currencyName.innerHTML="Dólar americano"
         currencyImg.src ="./img/eua.svg"
     }
-    if(select.value==='€ Euro'){
+    if(select.value==='euro'){
         currencyName.innerHTML="Euro"
         currencyImg.src ="./img/UE.svg"
     }
-    if(select.value==='Bitcoin'){
+    if(select.value==='bitcoin'){
         currencyName.innerHTML="Bitcoin"
         currencyImg.src ="./img/Btc.png"
     }
